bdf2da3d6d217c8546029348246cd11fd0a5142c
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-02-08 17:07:56 +0200

    create fetch function and render dom in html Relates #11

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const play = selector(".play");
 
+const surahContainer = (data) => {
+  selector(".ayah-text").textContent = data.data.verses[0].text.arab;
+  selector(".ayah-trans").textContent = data.data.verses[0].translation.en;
+  selector(".audio").src = data.data.verses[0].audio.primary;
+  selector(".surah-name").textContent = data.data.name.long;
+};
+
+play.addEventListener("click", () => {
+  const sound = selector(".audio");
+  sound.play();
+});
+
+fetch(surah, surahContainer);
