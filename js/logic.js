b8c6b85797da71e5d0d3a8eaf41e5092c2a0d20c
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-02-08 20:55:32 +0200

    create play-pause functionality and render msg error Relates #13

--- js/logic.js
+++ js/logic.js
@@ -1,26 +1,34 @@
-const quranSurahs = 'https://api.quran.sutanlab.id/surah';
-const surah = 'https://api.quran.sutanlab.id/surah/108';
+const quranSurahs = "https://api.quran.sutanlab.id/surah";
+const surah = "https://api.quran.sutanlab.id/surah/108";
 
 const fetch = (url, cb) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            switch (xhr.status) {
-                case 200:
-                    let data = JSON.parse(xhr.responseText);
-                    cb(data);
-                    break;
-                case 404:
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
-    xhr.open('GET', url);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      switch (xhr.status) {
+        case 200:
+          let data = JSON.parse(xhr.responseText);
+          cb(data);
+          break;
+        case 404:
+          renderError();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
 };
 
 const selector = (selector) => {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
 };
+
+function renderError(){
+  const body = document.body;
+  body.innerHTML = `<div class="error-img">
+  <img src="https://http.cat/404"/>
+  </div>`
+}
--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
-const ayahText = selector('.ayah-text');
-const ayahTrans = selector('.ayah-trans');
-const surahName = selector('.surah-name');
-const audio = selector('.audio');
+const ayahText = selector(".ayah-text");
+const ayahTrans = selector(".ayah-trans");
+const surahName = selector(".surah-name");
+const audio = selector(".audio");
 
-const playBtn = selector('.play');
-const playPauseIcon = selector('.play i');
-const next = selector('.next');
-const back = selector('.back');
+const playBtn = selector(".play");
+const playPauseIcon = selector(".play i");
+const next = selector(".next");
+const back = selector(".back");
 
 const surahContainer = (data) => {
-    let verses = data.data.verses;
+  let verses = data.data.verses;
 
-    let surahTag = data.data.name.long;
-    let ayahAudios = [];
-    let ayahTexts = [];
-    let transAyahTexts = [];
+  let surahTag = data.data.name.long;
+  let ayahAudios = [];
+  let ayahTexts = [];
+  let transAyahTexts = [];
 
-    verses.forEach((verse) => {
-        ayahAudios.push(verse.audio.primary);
-        ayahTexts.push(verse.text.arab);
-        transAyahTexts.push(verse.translation.en);
-    });
+  verses.forEach((verse) => {
+    ayahAudios.push(verse.audio.primary);
+    ayahTexts.push(verse.text.arab);
+    transAyahTexts.push(verse.translation.en);
+  });
 
-    let ayahIndex = 0;
+  let ayahIndex = 0;
+  changeAyah(ayahIndex);
+
+  audio.addEventListener("ended", () => {
+    ayahIndex++;
     changeAyah(ayahIndex);
-
-    audio.addEventListener('ended', () => {
-        ayahIndex++;
-        changeAyah(ayahIndex);
-    });
-
-    function changeAyah(ayahNo) {
-        if (ayahNo < ayahAudios.length) {
-            audio.src = ayahAudios[ayahNo];
-            ayahText.innerText = ayahTexts[ayahNo];
-            ayahTrans.innerText = transAyahTexts[ayahNo];
-            surahName.innerText = surahTag;
-            audio.play();
-        }
+  });
+  window.onload = () => {
+    audio.play();
+  };
+  function changeAyah(ayahNo) {
+    if (ayahNo < ayahAudios.length) {
+      audio.src = ayahAudios[ayahNo];
+      ayahText.innerText = ayahTexts[ayahNo];
+      ayahTrans.innerText = transAyahTexts[ayahNo];
+      surahName.innerText = surahTag;
+      audio.play();
     }
+  }
 };
+let state = true;
+playBtn.addEventListener("click", () => {
+  if (state === true) {
+    playPauseIcon.classList.add("fa-play");
+    state = false;
+    audio.pause();
+  } else {
+    playPauseIcon.classList.remove("fa-play");
+    playPauseIcon.classList.add("fa-pause");
+    state = true;
+    audio.play();
+  }
+});
 
 fetch(surah, surahContainer);
