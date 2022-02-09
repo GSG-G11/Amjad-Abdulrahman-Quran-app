945dff2505cd0de4ba967644582d1157ee832b51
Amjad Yahia Roben Hassan <amjedyehia1998@gmail.com>
2022-02-09 08:23:41 +0200

    add next-previous ayah feature relates #13

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,6 @@
-const quranSurahs = "https://api.quran.sutanlab.id/surah";
-const surah = "https://api.quran.sutanlab.id/surah/104";
+const randomSurah = getRandomSurah();
+const quranSurahs = 'https://api.quran.sutanlab.id/surah';
+const surah = `https://api.quran.sutanlab.id/surah/${randomSurah}`;
 
 const fetch = (url, cb) => {
   const xhr = new XMLHttpRequest();
@@ -8,20 +9,58 @@ const fetch = (url, cb) => {
       switch (xhr.status) {
         case 200:
           let data = JSON.parse(xhr.responseText);
-          console.log(data);
           cb(data);
           break;
         case 404:
+          renderError();
           break;
         default:
           break;
       }
     }
   };
-  xhr.open("GET", url);
+  xhr.open('GET', url);
   xhr.send();
 };
 
 const selector = (selector) => {
   return document.querySelector(selector);
 };
+
+function renderError() {
+  const body = document.body;
+  body.innerHTML = `<div class="error-img">
+  <img src="https://http.cat/404"/>
+  </div>`;
+}
+
+function getRandomSurah() {
+  return Math.floor(Math.random() * 115);
+}
+
+const getDate = () => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date().toLocaleDateString('en-us', options);
+};
+
+const getWeekday = () => {
+  const currentDate = new Date();
+  var options = { weekday: 'long' };
+  return new Intl.DateTimeFormat('en-US', options).format(currentDate);
+};
+
+const getTime = () => {
+  return new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+};
+
+// todo surah name translation
+// todo play btn on first click doesn't change bug
+// todo header responsive
+// main section ayah width
--- js/main.js
+++ js/main.js
@@ -1,15 +1,98 @@
-const play = selector(".play");
+const ayahText = selector('.ayah-text');
+const ayahTrans = selector('.ayah-trans');
+const surahName = selector('.surah-name');
+const audio = selector('.audio');
+const playBtn = selector('.play');
+const playPauseIcon = selector('.play i');
+const nextBtn = selector('.next');
+const backBtn = selector('.back');
+
+const fullDate = selector('.date-fullDate');
+const day = selector('.date-day');
+const time = selector('.time');
+
+const speakerIcon = selector('.app-speaker-icon i');
+
+fullDate.innerText = getDate();
+day.innerText = getWeekday();
+time.innerText = getTime();
 
 const surahContainer = (data) => {
-  selector(".ayah-text").textContent = data.data.verses[0].text.arab;
-  selector(".ayah-trans").textContent = data.data.verses[0].translation.en;
-  selector(".audio").src = data.data.verses[0].audio.primary;
-  selector(".surah-name").textContent = data.data.name.long;
+  let verses = data.data.verses;
+
+  let surahTag = data.data.name.long;
+  let ayahAudios = [];
+  let ayahTexts = [];
+  let transAyahTexts = [];
+  let ayahIndex = 0;
+
+  verses.forEach((verse) => {
+    ayahAudios.push(verse.audio.primary);
+    ayahTexts.push(verse.text.arab);
+    transAyahTexts.push(verse.translation.en);
+  });
+
+  // start ayah on initial click
+  changeAyah(ayahIndex);
+
+  // change ayah automatically
+  audio.addEventListener('ended', () => {
+    ayahIndex++;
+    changeAyah(ayahIndex);
+  });
+
+  // Next Ayah
+  nextBtn.addEventListener('click', () => {
+    lastAyah = ayahIndex > ayahAudios.length;
+    !lastAyah && changeAyah(++ayahIndex);
+  });
+
+  // Previous Ayah
+  backBtn.addEventListener('click', () => {
+    firstAyah = ayahIndex === 0;
+    !firstAyah && changeAyah(--ayahIndex);
+  });
+
+  function changeAyah(ayahNo) {
+    if (ayahNo < ayahAudios.length) {
+      audio.src = ayahAudios[ayahNo];
+      ayahText.innerText = ayahTexts[ayahNo];
+      ayahTrans.innerText = transAyahTexts[ayahNo];
+      surahName.innerText = surahTag;
+    }
+  }
 };
 
-play.addEventListener("click", () => {
-  const sound = selector(".audio");
-  sound.play();
+// Toggle play state
+let playState = true;
+playBtn.addEventListener('click', () => {
+  if (playState === true) {
+    playPauseIcon.classList.add('fa-play');
+    playState = false;
+    audio.pause();
+  } else {
+    playPauseIcon.classList.remove('fa-play');
+    playPauseIcon.classList.add('fa-pause');
+    playState = true;
+    audio.play();
+  }
+});
+
+// Toggle mute state
+let muteState = false;
+speakerIcon.addEventListener('click', (e) => {
+  if (muteState === false) {
+    speakerIcon.classList.add('fa-volume-mute');
+    audio.muted = true;
+
+    muteState = true;
+  } else {
+    speakerIcon.classList.remove('fa-volume-mute');
+    speakerIcon.classList.add('fa-volume-up');
+    audio.muted = false;
+
+    muteState = false;
+  }
 });
 
 fetch(surah, surahContainer);
