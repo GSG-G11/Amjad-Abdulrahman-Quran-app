7fcf14b2357030402ce4de96459f788f40e59787
Amjad Yahia <amjedyehia1998@gmail.com>
2022-03-06 21:25:36 +0200

    removed unneccesa commentedy code

--- public/js/request.js
+++ public/js/request.js
@@ -1,19 +1,3 @@
-// import { renderError } from './logic.js';
-
-// const fetch = (url, cb) => {
-//   const xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = () => {
-//     if (xhr.status === 200) {
-//       const data = JSON.parse(xhr.responseText || '{}');
-//       return cb(data);
-//     }
-
-//     if (xhr.status === 404) return renderError();
-//   };
-//   xhr.open('GET', url);
-//   xhr.send();
-// };
-
 const fetchData = async (url, method, dataToSend) => {
   const res = await fetch(url, {
     method,
